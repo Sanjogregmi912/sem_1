5a27a8e5ae79a44d00f5175913eba829e2cb8249
Sanjogregmi912 <78246727+Sanjogregmi912@users.noreply.github.com>
2021-05-07 18:17:01 +0545

    login validated

--- ems/ems.py
+++ ems/ems.py
@@ -1,24 +1,23 @@
 ﻿from tkinter import *
-from tkinter import Tk
 from tkcalendar import Calendar
 import sqlite3
 from tkinter import messagebox
 from PIL import Image,ImageTk
+from functools import partial
 root=Tk()
 root.title("employee management system")
 root.iconbitmap("top.ico")
-root.geometry("500x400")
+root.geometry("495x651")
 bg=ImageTk.PhotoImage(Image.open("background.jpg"))
 label_image=Label(root,image=bg).place(x=0,y=0)
 #_____________________________________
 frame_title=Frame(root,bg="blue")
 frame_title.grid(row=1,column=1)
 
-title_label=Label(frame_title,text="\tWELCOME TO THE \n \t EMPLOYEE MANAGEMENT SYSTEM \n\t FOR THE EMPLOYEE IN THE COMPANY \t",font="vandana,36",width=50,height=6)
+title_label=Label(frame_title,text="\tWELCOME TO THE \n\tEMPLOYEE MANAGEMENT SYSTEM\n\t FOR THE EMPLOYEE IN THE COMPANY \t",font="vandana,36",width=50,height=6)
 title_label.grid(row=0,column=0)
 
-button_login=Button(frame_title,text="LOGIN",height=3,width=30)
-button_login.grid(row=1,column=0)
+
 
 
 
@@ -34,7 +33,7 @@ def register():
         c = connect.cursor()
         # insert to the table
         c.execute(
-            "INSERT INTO registration VALUES( :First_name, :Last_name,:gender, :address, :citizenship_number, :nationality, :experience, :father_name,:phone_number,:email_address,:password)",
+            "INSERT INTO registration VALUES( :First_name, :Last_name,:gender, :address, :citizenship_number, :nationality, :experience, :father_name,:phone_number,:username,:password)",
             {
                 "First_name": first_name.get(),
                 "Last_name": last_name.get(),
@@ -45,7 +44,7 @@ def register():
                 "nationality": nationality.get(),
                 "father_name": father_name.get(),
                 "phone_number": phone_number.get(),
-                "email_address": email_address.get(),
+                "username": username_entry.get(),
                 "password": password.get()
 
             })
@@ -62,7 +61,7 @@ def register():
         nationality.delete(0, END)
         father_name.delete(0, END)
         phone_number.delete(0, END)
-        email_address.delete(0, END)
+        username_entry.delete(0, END)
         password.delete(0, END)
 
 
@@ -142,8 +141,8 @@ def register():
     phone_number_label = Label(frame1, text="Phone Number")
     phone_number_label.grid(row=5, column=4)
 
-    email_address_label = Label(frame1, text="Email Address")
-    email_address_label.grid(row=6, column=0)
+    username_label= Label(frame1, text=" username")
+    username_label.grid(row=6, column=0)
 
     password_label = Label(frame1, text="Password")
     password_label.grid(row=6, column=4)
@@ -174,8 +173,8 @@ def register():
     phone_number = Entry(frame1, width=30)
     phone_number.grid(row=5, column=5)
 
-    email_address = Entry(frame1, width=30)
-    email_address.grid(row=6, column=1)
+    username_entry = Entry(frame1, width=30)
+    username_entry.grid(row=6, column=1)
 
     password = Entry(frame1, width=30)
     password.grid(row=6, column=5)
@@ -263,7 +262,7 @@ def register():
             nationality text,
             experience text,
             phone_number integer,
-            email_address text,
+            username text,
             father_name text,
             password text
 
@@ -277,4 +276,69 @@ def register():
 
 button_register=Button(frame_title,text="REGISTER",height=3,width=30,command=register)
 button_register.grid(row=2,column=0)
+
+
+
+
+#______________________________________________
+#inside the login button
+def login():
+    top3=Toplevel()
+    top3.title("login page")
+    top3.geometry("300x300")
+    def vadlidate_login(username1,password):
+        print("Email:",username1.get())
+        print("password:",password.get())
+        return
+
+    usernameLabel = Label(top3, text="Email").grid(row=2, column=1)
+    username1=StringVar()
+    username2 = Entry(top3,width=20,textvariable=username1).grid(row=2, column=2,padx=5,pady=5)
+
+    password=StringVar()
+    passwordLabel = Label(top3, text="Password").grid(row=3, column=1)
+    passwordentry = Entry(top3, show='*',textvariable=password).grid(row=3, column=2,padx=5,pady=5)
+    vadlidate_login =partial(vadlidate_login,username1,password)
+
+
+
+
+    def addtodb():
+        connect = sqlite3.connect("registration form")
+        c = connect.cursor()
+        c.execute("SELECT *, oid FROM registration")
+        records = c.fetchall()
+        # print(records)
+        print_records = ""
+        print_records2=""
+        for record in records:
+            print_records += str(record[9])
+        for record2 in records:
+            print_records2+=str(record[10])
+        get_username=username1.get()
+        get_password=password.get()
+        if print_records==get_username and print_records2==get_password:
+
+            print("valid")
+
+        else:
+            print("invalid")
+
+
+
+
+
+        connect.commit()
+        connect.close()
+
+
+
+
+    show_button=Button(top3,text="login in",command=vadlidate_login and addtodb)
+    show_button.grid(row=4,column=0)
+    top3.mainloop()
+
+
+button_login=Button(frame_title,text="LOGIN ",height=3,width=30,command=login)
+button_login.grid(row=1,column=0)
 mainloop()
